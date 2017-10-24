96092ef88672c9f96cf1ac9e31f309f126fc9e37
Vadim Avdeev <vadim@virgilsecurity.com>
2017-10-24 19:27:47 +0300

    add basic health check endpoint

---
+++ controllers/health.js
@@ -0,0 +1,5 @@
+module.exports = {
+	status(req, res) {
+		res.status(200).send();
+	}
+};
--- services/router.js
+++ services/router.js
@@ -4,9 +4,15 @@ const requireAuth = require('./auth');
 const loadVirgilCard = require('./loadVirgilCard');
 const userController = require('../controllers/users');
 const tokenController = require('../controllers/tokens');
+const healthController = require('../controllers/health');
 
 router.route('/users').post(userController.register);
-router.route('/tokens/twilio')
-	.get(requireAuth(), loadVirgilCard(), tokenController.getTwilioToken);
+router.route('/tokens/twilio').get(
+	requireAuth(),
+	loadVirgilCard(),
+	tokenController.getTwilioToken
+);
+router.route('/health/status').get(healthController.status);
+
 
 module.exports = router;
