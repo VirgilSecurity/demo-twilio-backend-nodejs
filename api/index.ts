36154be67062393de59c7449e88429143795c279
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-07-17 16:41:35 +0300

    enable cors

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import router from "./routes";
 
+const enableCors: express.RequestHandler = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
+
 const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
     res.status(500);
     res.render("error", { error: err });
@@ -8,8 +14,9 @@ const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
 
 const app = express();
 
-app.use(errorHandler);
+app.use(enableCors);
 app.use(express.json());
 app.use('/', router);
+app.use(errorHandler);
 
 export default app;
