43d8913412b1ab39bcbcc43e866a4a2608e145a2
Alexey Smirnov <alexey@smirnov.works>
2018-12-19 17:30:08 +0200

    remove old files

--- api/controllers/getVirgilJwt.ts
+++
@@ -1,6 +0,0 @@
-import { RequestHandler } from "express";
-import { generator } from "../services/virgil";
-
-export const getVirgilJwt: RequestHandler = (req, res) => {
-    res.json({ token: generator.generateToken(req.body.identity).toString() });
-}
