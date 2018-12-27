b59e56562617191fa970551288ab2fc40efa9c95
Alexey Smirnov <alexey@smirnov.works>
2018-12-27 16:25:24 +0200

    update client

---
+++ public/controller.js
@@ -0,0 +1,76 @@
+loginOrRegister()
+    .then(firstUserData => joinOrCreateChannel(firstUserData))
+    .then(firstUserData => subscribeOnMessage(firstUserData))
+    .then(secondUserData => loginOrRegister(secondUserData.identity)
+    .then(secondUserData => joinOrCreateChannel(secondUserData))
+    .then(secondUserData => send(secondUserData)));
+
+function loginOrRegister(prevIdentity) {
+    let promptMessage = `type identity of the ${prevIdentity ? 'second' : 'first'} user`;
+    const identity = prompt(promptMessage).trim().toLowerCase();
+    if (prevIdentity === identity) {
+        return loginOrRegister('identity shouldn\'t be the same!');
+    }
+    return initialize(identity).then(clients => {
+        return clients.e3kit.register()
+            .then(() => {
+                displayMessage('register', identity);
+            })
+            .catch(e => {
+                if (e.name === 'IdentityAlreadyExistsError') {
+                    displayMessage('login', `${identity} is already registered`);
+                } else {
+                    console.log('oops, unexpected error:');
+                    console.error(e);
+                    return clients.e3kit.rotatePrivateKey();
+                }
+            }).then(() => ({ identity, ...clients }))
+    });
+}
+
+function joinOrCreateChannel({ identity, e3kit, twilioChat }) {
+    return getChannel(twilioChat, 'general').then((channel) => {
+        if (channel) {
+            return joinChannel(twilioChat, 'general').catch(() => {
+                displayMessage(identity, 'user already in channel');
+                return channel;
+            });
+        } else {
+            displayMessage('creating channel');
+            return createChannel(twilioChat, 'general');
+        }
+    }).then(channel => {
+        displayMessage(identity, 'joined to channel');
+        return { identity, e3kit, twilioChat, channel };
+    });
+}
+
+function subscribeOnMessage({ channel, identity, e3kit, twilioChat }) {
+    channel.on('messageAdded', async message => {
+        displayMessage('encrypted message received', message.body);
+        try {
+            let decryptedMessage = await decryptMessage(e3kit, message);
+            displayMessage('message decrypted', decryptedMessage)
+        } catch (error) {
+            console.log(error);
+            displayMessage('decryption error', error)
+        }
+    });
+    return { identity, e3kit, twilioChat };
+}
+
+function send({ identity, channel, e3kit }) {
+    const message = prompt('type message here');
+    displayMessage(identity + ' sends message', message);
+    return sendMessage(e3kit, channel, message);
+}
+
+function displayMessage(header, text) {
+    const p = document.createElement('p');
+    const author = document.createElement('b');
+    author.innerText = header + ': ';
+    const body = document.createTextNode(text);
+    p.appendChild(author);
+    if (text) p.appendChild(body);
+    document.body.appendChild(p);
+}
---
+++ public/snippets.js
@@ -0,0 +1,99 @@
+// This function returns a token that will be used to authenticate requests
+// to your backend.
+// This is a simplified solution without any real protection, so here you need use your
+// application authentication mechanism.
+async function authenticate(identity) {
+    const response = await fetch('http://localhost:3000/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            identity: identity
+        })
+    });
+    if (!response.ok) {
+        throw new Error(`Error code: ${response.status} \nMessage: ${response.statusText}`);
+    }
+    return response.json().then(data => data.authToken);
+}
+
+// This function makes authenticated request to GET /virgil-jwt endpoint
+// The token serves to make authenticated requests to Virgil Cloud
+async function getVirgilToken(authToken) {
+    const response = await fetch('http://localhost:3000/virgil-jwt', {
+        headers: {
+            // We use bearer authorization, but you can use any other mechanism.
+            // The point is only, this endpoint should be protected.
+            Authorization: `Bearer ${authToken}`,
+        }
+    })
+    if (!response.ok) {
+        throw new Error(`Error code: ${response.status} \nMessage: ${response.statusText}`);
+    }
+
+    // If request was successful we return Promise which will resolve with token string.
+    return response.json().then(data => data.virgilToken);
+}
+
+// This function makes authenticated request to GET /twilio-jwt endpoint
+// Returned token is used by twilio library
+async function getTwilioToken(authToken) {
+    const response = await fetch('http://localhost:3000/twilio-jwt', {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        }
+    })
+    if (!response.ok) {
+        throw new Error(`Error code: ${response.status} \nMessage: ${response.statusText}`);
+    }
+    return response.json().then(data => data.twilioToken);
+}
+
+async function initialize(identity) {
+    // E3kit will call this callback function and wait for the Promise resolve.
+    // When it receives Virgil JWT it can do authorized requests to Virgil Cloud.
+    // E3kit uses the identity encoded in the JWT as the current user's identity.
+    const authToken = await authenticate(identity);
+    const [e3kit, twilioChat] = await Promise.all([
+        E3kit.EThree.initialize(() => getVirgilToken(authToken)),
+        getTwilioToken(authToken).then(twilioToken => Twilio.Chat.Client.create(twilioToken))
+    ]);
+    return { e3kit, twilioChat };
+}
+
+async function createChannel(twilioChat, name) {
+    return await twilioChat.createChannel({
+        uniqueName: name,
+        friendlyName: name,
+    });
+}
+
+async function joinChannel(twilioChat, name) {
+    const channel = await getChannel(twilioChat, name);
+    return channel.join();
+}
+
+async function getChannel(twilioChat, name) {
+    const paginator = await twilioChat.getPublicChannelDescriptors();
+    for (i = 0; i < paginator.items.length; i++) {
+        const channelDescriptor = paginator.items[i];
+        if (channelDescriptor.uniqueName === name) {
+            const channel = await channelDescriptor.getChannel();
+            return channel;
+        }
+    }
+    return null;
+}
+
+async function decryptMessage(e3kit, message) {
+    const authorPublicKey = await e3kit.lookupPublicKeys(message.author);
+    return await e3kit.decrypt(message.body, authorPublicKey);
+}
+
+async function sendMessage(e3kit, channel, message) {
+    const membersIdentities = await channel.getMembers().then(members => members.map(member => member.identity));
+    const publicKeys = await e3kit.lookupPublicKeys(membersIdentities);
+    const encryptedMessage = await e3kit.encrypt(message, publicKeys);
+    return channel.sendMessage(encryptedMessage);
+}
